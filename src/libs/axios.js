70ff04047de830e041bdb0bfdac9ed762a224f46
shafeeque ahmad <shafeeq.ahmed541@gmail.com>
2022-05-24 18:35:17 +0500

    Update axios.js

--- src/libs/axios.js
+++ src/libs/axios.js
@@ -6,8 +6,8 @@ import axios from 'axios'
 const axiosIns = axios.create({
   // You can add your headers here
   // ================================
-  baseURL: 'http://127.0.0.1:8000/api',
-  // baseURL: 'https://backend.MYFILE.ai/api',
+  // baseURL: 'http://127.0.0.1:8000/api',
+  baseURL: 'https://myfile-backend.mcvdigital.ca/api',
 
   // timeout: 1000,
   // headers: {'X-Custom-Header': 'foobar'}
